555c8b858aaf6b90a1be5300dcfa828cc68a66c7
IndrakumarRC <indrakumar.sekaran@anywhere.co>
2019-12-25 14:16:58 +0530

    Added descriptions and sampleData
    * Added past_task indicator.
    * Added method descriptions for readability.

--- src/main/webapp/javascript/main.js
+++ src/main/webapp/javascript/main.js
@@ -1,4 +1,9 @@
 (function (_global) {
+    /**
+     * @description This method will give the details of the given time(in milliSeconds)
+     * @param {*} timeInMillis 
+     * @returns Object
+     */
     const getTimeDetails = timeInMillis => {
         if (!timeInMillis) return {}
         let dateObj = new Date(timeInMillis),
@@ -16,14 +21,39 @@
             isToday: (Number(splittedDate[2]) === new Date().getDate())
         };
     };
-    const scheduleWrapper = (eventDesc, fromTime, toTime) => {
-        let schedule_title = !toTime ? "All Day" : `${fromTime} - ${toTime}`
+
+    /**
+     * @description This method will return true,
+     * if the given time(time in milliSeconds) is the past one, else return false
+     * @param {*} timeInMillis 
+     * @returns Boolean
+     */
+    const isPast = timeInMillis => {
+        return timeInMillis < new Date().getTime() ? true : false;
+    }
+
+    /**
+     * @description This method will wrap the schedules for the day, inside the HTML_template
+     * @param {*} schedule Object
+     * @param {*} fromTime Concatenated fromTime in String [Ex: 12:10pm]
+     * @param {*} toTime Concatenated toTime in String [Ex: 12:20pm]
+     * @returns HTML_template string with appended given details of the schedule
+     */
+    const scheduleWrapper = (schedule, fromTime, toTime) => {
+        let schedule_title = !toTime ? "All Day" : `${fromTime} - ${toTime}`,
+            dot_classType = isPast(schedule.timeInMillis_To) ? ` past_task` : ``;
         return `<p class="schedule_wrap">
-            <span class="schedule_dot"></span>
+            <span class="schedule_dot${dot_classType}"></span>
             <span class="schedule_title">${schedule_title}</span>
-            <span class="schedule_desc">${eventDesc}</span>
+            <span class="schedule_desc">${schedule.eventDesc}</span>
             </p>`;
     };
+
+    /**
+     * @description This method will construct the given schedules present for the day
+     * @param {*} schedule Object
+     * @returns Accumulated string of HTML_template with the given details of the schedule
+     */
     const wrapSchedule = schedule => {
         if (schedule) {
             return schedule.reduce((accumulator, currentValue) => {
@@ -31,13 +61,21 @@
                     toTime_Details = getTimeDetails(currentValue.timeInMillis_To);
                 return accumulator +
                     scheduleWrapper(
-                        currentValue.eventDesc,
+                        currentValue,
                         fromTime_Details.concatenatedString,
                         toTime_Details.concatenatedString
                 )
             }, ``);
         }
     };
+
+    /**
+     * @description This method will bind the day, and the schedules for the particular day together, 
+     * inside the HTML_template
+     * @param {*} schedule Object
+     * @param {*} timeInMillis time in milliSeconds
+     * @returns HTML_template string with appended given details of the schedule
+     */
     const getScheduleTemplate = (schedule, timeInMillis) => {
         if (schedule && schedule.length <= 0) {
             return ``;
@@ -54,9 +92,19 @@
             </td>
         </tr>`;
     };
-    let resultantValue = ``;
-    Object.keys(schedules).forEach(item => {
-        resultantValue += getScheduleTemplate(schedules[item], Number(item));
-    });
-    document.getElementById('schedules').querySelector('tbody').innerHTML = resultantValue;
+
+    /**
+     * @description This method will paint the schedules list in the DOM
+     */
+    const paintSchedule = () => {
+        let resultantValue = ``;
+        Object.keys(schedules).forEach(item => {
+            // Here we are wrapping up the schedules, inside the HTML_template
+            resultantValue += getScheduleTemplate(schedules[item], Number(item));
+        });
+        document.getElementById('schedules').querySelector('tbody').innerHTML = resultantValue;
+    }
+
+    // Painting the schedule list
+    paintSchedule();
 })(this);
--- src/main/webapp/javascript/sampleData.js
+++ src/main/webapp/javascript/sampleData.js
@@ -72,4 +72,126 @@ var schedules = Object.freeze({
             eventDesc: "Appointment Scheduling"
         }
     ],
+    1577431800000: [
+        {
+            timeInMillis_From: 1577431800000,
+            timeInMillis_To: 1577433000000,
+            eventDesc: "Client Meeting"
+        }
+    ],
+    1577519400000: [
+        {
+            timeInMillis_From: 1577519400000,
+            timeInMillis_To: 1577520600000,
+            eventDesc: "US Meeting for App Enhancements"
+        }
+    ],
+    1577615400000: [
+        {
+            timeInMillis_From: 1577615400000,
+            timeInMillis_To: 1577619000000,
+            eventDesc: "Team Review Meeting"
+        }
+    ],
+    1577680200000: [
+        {
+            timeInMillis_From: 1577680200000,
+            timeInMillis_To: 1577683800000,
+            eventDesc: "Application - Live Deployment - CloudRun"
+        }
+    ],
+    1577746800000: [
+        {
+            timeInMillis_From: 1577746800000,
+            timeInMillis_To: 1577750400000,
+            eventDesc: "Run Automation for G_Schedules App"
+        }
+    ],
+    1577853000000: [
+        {
+            timeInMillis_From: 1577853000000,
+            timeInMillis_To: undefined,
+            eventDesc: "New Year!!!"
+        }
+    ],
+    1577853000000: [
+        {
+            timeInMillis_From: 1577853000000,
+            timeInMillis_To: 1577854800000,
+            eventDesc: "New Year Celebration"
+        }
+    ],
+    1577941200000: [
+        {
+            timeInMillis_From: 1577941200000,
+            timeInMillis_To: 1577942400000,
+            eventDesc: "Push static contents to CDN"
+        }
+    ],
+    1578025800000: [
+        {
+            timeInMillis_From: 1578025800000,
+            timeInMillis_To: 1578027600000,
+            eventDesc: "Gadget sheet update"
+        }
+    ],
+    1578117600000: [
+        {
+            timeInMillis_From: 1578117600000,
+            timeInMillis_To: 1578118800000,
+            eventDesc: "Update, gadgets for the team"
+        }
+    ],
+    1578220200000: [
+        {
+            timeInMillis_From: 1578220200000,
+            timeInMillis_To: 1578222000000,
+            eventDesc: "New Interns Review"
+        }
+    ],
+    1578308400000: [
+        {
+            timeInMillis_From: 1578308400000,
+            timeInMillis_To: 1578309000000,
+            eventDesc: "Publish offer letter for newly joined employees"
+        }
+    ],
+    1578395400000: [
+        {
+            timeInMillis_From: 1578395400000,
+            timeInMillis_To: 1578399000000,
+            eventDesc: "Set up a new back-end team"
+        }
+    ],
+    1578460200000: [
+        {
+            timeInMillis_From: 1578460200000,
+            timeInMillis_To: 1578463800000,
+            eventDesc: "Organize Sprint Review"
+        }
+    ],
+    1578547800000: [
+        {
+            timeInMillis_From: 1578547800000,
+            timeInMillis_To: 1578551400000,
+            eventDesc: "Check testing report and make live deployment"
+        }
+    ],
+    1578637800000: [
+        {
+            timeInMillis_From: 1578637800000,
+            timeInMillis_To: 1578639600000,
+            eventDesc: "Sprint Retrospective and Sprint Planning"
+        },
+        {
+            timeInMillis_From: 1578600000000,
+            timeInMillis_To: 1578603600000,
+            eventDesc: "Sprint Planning"
+        },
+        {
+            timeInMillis_From: 1578607200000,
+            timeInMillis_To: 1578610800000,
+            eventDesc: "Sprint Tasks Allocation and Points table update"
+        }
+    ]
 });
